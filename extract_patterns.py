97ff20f5a0da342e0c3ea4554cc9dd4f91a87676
bavagliladri <lucabrigadavilla@gmail.com>
2022-05-27 09:58:58 +0200

    initial commit

--- extract_patterns.py
+++ extract_patterns.py
@@ -1,7 +1,10 @@
 import udapi
 import pandas as pd
+from itertools import combinations
+
 
 def pattern1(tb):
+    results = []
     # iterate over sentence of the treebank
     for sent in tb:
         root = sent.get_tree()
@@ -12,7 +15,25 @@ def pattern1(tb):
             if verb.upos == 'VERB':
                 # check if the verb is finite
                 if 'VerbForm' not in verb.feats or verb.feats['VerbForm'] not in ['Conv', 'Gdv', 'Ger', 'Inf', 'Part', 'Sup', 'Vnoun']:
-                    pass
+                    # create a list with all the nodes whose parent is the verb restricting to those nodes who precedes the verb
+                    prec_children = verb.children(preceding_only=True)
+                    # iterate over the combinations of two preceding children
+                    for pair in combinations(prec_children, 2):
+                        # looking for a subject and object
+                        if pair[0].deprel == 'nsubj' and pair[0].feats['Case'] == 'Nom' and \
+                                pair[1].deprel == 'obj' and pair[1].feats['Case'] == 'Acc' and \
+                                pair[0].precedes(pair[1]):
+                            subj = pair[0]
+                            obj = pair[1]
+                            results.append({'subj': subj, 'obj': obj, 'verb': verb})
+                        elif pair[1].deprel == 'nsubj' and pair[1].feats['Case'] == 'Nom' and \
+                                pair[0].deprel == 'obj' and pair[0].feats['Case'] == 'Acc' and \
+                                pair[1].precedes(pair[1]):
+                            subj = pair[1]
+                            obj = pair[0]
+                            results.append({'subj': subj, 'obj': obj, 'verb': verb})
+    return results
 
 
-# tb = udapi.Document('agdt_synsets.conllu')
+if __name__ == '__main__':
+    tb = udapi.Document('agdt_synsets.conllu')
